457b859af5226cb6f9522bf0450b19bd17ff71ea
tbrosh <tbrosh23@aol.com>
2020-11-01 21:38:58 -0500

    initial commit

---
+++ toolbar_with_scale.py
@@ -0,0 +1,44 @@
+import wx
+
+def scale_bitmap(bitmap, width, height):
+    image = wx.ImageFromBitmap(bitmap)
+    image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+    result = wx.BitmapFromImage(image)
+    return result
+
+
+class Example(wx.Frame):
+
+    def __init__(self, *args, **kwargs):
+        super(Example, self).__init__(*args, **kwargs)
+
+        self.InitUI()
+
+    def InitUI(self):
+
+        toolbar = self.CreateToolBar()
+        bitmap = wx.Bitmap('exit.png')
+        bitmap = scale_bitmap(bitmap,50,50)
+        qtool = toolbar.AddTool(wx.ID_ANY, 'Quit', bitmap)
+        toolbar.Realize()
+
+        self.Bind(wx.EVT_TOOL, self.OnQuit, qtool)
+
+        self.SetSize((350, 250))
+        self.SetTitle('Simple toolbar')
+        self.Centre()
+
+    def OnQuit(self, e):
+        self.Close()
+
+
+def main():
+
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
